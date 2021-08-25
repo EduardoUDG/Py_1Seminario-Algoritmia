a38f30be9170fe25bf29bacb648ef9a37e50f3e6
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2021-08-25 09:23:51 -0500

    Lectura de consola e impresion

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 
+# Los saltos de linea indican punto y coma
+# No hay llaves para separar código
+
 a = 10
 b = 15.5
 nombre = "Ricardo"
 ap = "Perez"
+
+# Entrada de datos
+# print() ya incluye un salto de linea al final
+
+print("=== Welcome ===")
+name = input("Introduce tu nombre: ")
+age  = input("Edad: ")
+
+print("Hola", name, "tu edad es:", age)
+
+
 
 
